b5d5f344d26d704b4a396efe97f5c26010c0fab7
MarkCrossrath <cross672@gmail.com>
2017-04-18 14:25:45 +0100

    changing menu

--- app/src/main/java/com/example/markcross/assignment3/OptionsActivity.java
+++ app/src/main/java/com/example/markcross/assignment3/OptionsActivity.java
@@ -80,17 +80,17 @@ public class OptionsActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_percentge) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_distance) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_mapDistance) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_gradient) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_temperature) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_bmi) {
 
         }
 
